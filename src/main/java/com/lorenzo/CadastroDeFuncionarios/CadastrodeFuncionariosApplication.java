87648943bc01eefd12f333337754b25aa540365b
Lorenzo Callegari <lorenzo.luizcallegari05@gmail.com>
2025-09-08 22:15:44 -0300

    primeira commit

---
+++ src/main/java/com/lorenzo/CadastroDeFuncionarios/CadastrodeFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.lorenzo.CadastroDeFuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeFuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lorenzo/CadastroDeFuncionarios/CadastrodeFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenzo.CadastroDeFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
