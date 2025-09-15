0d0ef1eee72bba07022091827273a0521d473fc9
Lorenzo Callegari <lorenzo.luizcallegari05@gmail.com>
2025-09-15 00:06:46 -0300

    feat: Adiciona o Lombok as classes para abstracao

--- src/main/java/com/lorenzo/CadastroDeFuncionarios/Tasks/TasksModel.java
+++ src/main/java/com/lorenzo/CadastroDeFuncionarios/Tasks/TasksModel.java
@@ -2,11 +2,17 @@ package com.lorenzo.CadastroDeFuncionarios.Tasks;
 
 import com.lorenzo.CadastroDeFuncionarios.Employees.EmployeeModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TasksModel {
 
     @Id
@@ -20,6 +26,4 @@ public class TasksModel {
     // A task can have many employees
     @OneToMany(mappedBy = "tasks")
     private List<EmployeeModel> employees;
-
-
 }
