d23a8dcefe35462275caf1127bd4c1fa5128fed9
Lorenzo Callegari <lorenzo.luizcallegari05@gmail.com>
2025-09-28 12:59:14 -0300

    chore: inclui a versao do DTO pela entidade e mapeia manualmente no TasksMapper os caminhos entidade x dto

--- src/main/java/com/lorenzo/CadastroDeFuncionarios/Tasks/dto/TasksDTO.java
+++ src/main/java/com/lorenzo/CadastroDeFuncionarios/Tasks/dto/TasksDTO.java
@@ -1,4 +1,24 @@
 package com.lorenzo.CadastroDeFuncionarios.Tasks.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.lorenzo.CadastroDeFuncionarios.Employees.Model.EmployeeModel;
+import com.lorenzo.CadastroDeFuncionarios.Tasks.Model.TaskStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TasksDTO {
+
+    private Long id;
+    private String name;
+    private TaskStatus status;
+    private String description;
+    private List<EmployeeModel> employees;
+
 }
