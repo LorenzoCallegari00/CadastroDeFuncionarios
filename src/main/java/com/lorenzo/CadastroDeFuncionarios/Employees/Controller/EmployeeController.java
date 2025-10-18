e4a6562aa5a8254a491a428cdeef8198c2f13444
Lorenzo Callegari <lorenzo.luizcallegari05@gmail.com>
2025-10-18 00:45:39 -0300

    doc: adiciona swagger na minha aplicacao para documentacao nas minhas rotas nos Controllers de Employees e Tasks

--- src/main/java/com/lorenzo/CadastroDeFuncionarios/Employees/Controller/EmployeeController.java
+++ src/main/java/com/lorenzo/CadastroDeFuncionarios/Employees/Controller/EmployeeController.java
@@ -2,6 +2,9 @@ package com.lorenzo.CadastroDeFuncionarios.Employees.Controller;
 
 import com.lorenzo.CadastroDeFuncionarios.Employees.Service.EmployeesService;
 import com.lorenzo.CadastroDeFuncionarios.Employees.dto.EmployeeDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +23,10 @@ public class EmployeeController {
 
     // Add Employee (CREATE)
     @PostMapping
-    public ResponseEntity<String> addEmployee(@RequestBody EmployeeDTO dto) {
+    @Operation(summary = "Cria um novo Funcionario", description = "Rota cria novo Funcionario e insere no Banco de Dados")
+    @ApiResponse(responseCode = "201", description = "Funcionario Criado com Sucesso")
+    @ApiResponse(responseCode = "400", description = "Erro ao Criar Funcionario")
+    public ResponseEntity<String> addEmployee(@Parameter(description = "Usuario envia os dados do Funcionario a ser criado no corpo da requisicao") @RequestBody EmployeeDTO dto) {
         EmployeeDTO add = employeesService.addEmployee(dto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Funcionario: " + add.getName() + " criado com sucesso | ID: " + add.getId());
@@ -28,6 +34,10 @@ public class EmployeeController {
 
     // Show all Employees (READ)
     @GetMapping
+    @Operation(summary = "Mostra todos os Funcionarios cadastrados", description = "Rota mostra todos os Funcionarios salvos no Banco de Dados")
+    @ApiResponse(responseCode = "200", description = "Mostrando todos os Funcionarios")
+    @ApiResponse(responseCode = "404", description = "Nao ha Funcionarios Existentes")
+    @ApiResponse(responseCode = "400", description = "Erro ao Mostrar Todos os Funcionarios")
     public ResponseEntity<List<EmployeeDTO>> showAllEmployees() {
         List<EmployeeDTO> employeeDTOList = employeesService.showAllEmployees();
         return ResponseEntity.ok(employeeDTOList);
@@ -35,7 +45,10 @@ public class EmployeeController {
 
     // Search Employee by ID (READ)
     @GetMapping("/{id}")
-    public ResponseEntity<?> showEmployeeById(@PathVariable Long id) {
+    @Operation(summary = "Lista Funcionario pelo ID", description = "Rota Lista um Funcionario pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Funcionario Encontrado")
+    @ApiResponse(responseCode = "404", description = "Funcionario Nao Encontrado")
+    public ResponseEntity<?> showEmployeeById(@Parameter(description = "Usuario envia o ID no Path da requisicao") @PathVariable Long id) {
         EmployeeDTO findId = employeesService.showEmployeeById(id);
         if (findId != null) {
             return ResponseEntity.ok(findId);
@@ -46,7 +59,11 @@ public class EmployeeController {
 
     // Update Employee data (UPDATE)
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateEmployee(@RequestBody EmployeeDTO dto, @PathVariable Long id) {
+    @Operation(summary = "Atualiza Funcionario Existente", description = "Rota atualiza Funcionario no Banco de Dados pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Funcionario Atualizado com Sucesso")
+    @ApiResponse(responseCode = "404", description = "Funcionario Nao Encontrado")
+    @ApiResponse(responseCode = "400", description = "Erro ao Atualizar Funcionario")
+    public ResponseEntity<String> updateEmployee(@Parameter(description = "Usuario envia os dados do Funcionario a ser atualizado no corpo da requisicao") @RequestBody EmployeeDTO dto, @Parameter(description = "Usuario envia o ID no Path da requisicao") @PathVariable Long id) {
         if (employeesService.showEmployeeById(id) != null) {
             employeesService.updateEmployee(dto, id);
             return ResponseEntity.ok("Funcionario " + dto.getName() + " ID: " + id + " | Atualizado com sucesso");
@@ -57,7 +74,11 @@ public class EmployeeController {
 
     // Delete Employee (DELETE)
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> removeEmployee(@PathVariable Long id) {
+    @Operation(summary = "Remove um Funcionario", description = "Rota remove um Funcionario do Banco de Dados pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Funcionario Removido com Sucesso")
+    @ApiResponse(responseCode = "404", description = "Funcionario Nao Encontrado")
+    @ApiResponse(responseCode = "400", description = "Erro ao remover Funcionario")
+    public ResponseEntity<String> removeEmployee(@Parameter(description = "Usuario envia o ID no Path da requisicao") @PathVariable Long id) {
         if (employeesService.showEmployeeById(id) != null) {
             employeesService.removeEmployee(id);
             return ResponseEntity.ok("Funcionario de id: " + id + " | Deletado com sucesso");
--- src/main/java/com/lorenzo/CadastroDeFuncionarios/Tasks/Controller/TasksController.java
+++ src/main/java/com/lorenzo/CadastroDeFuncionarios/Tasks/Controller/TasksController.java
@@ -2,6 +2,9 @@ package com.lorenzo.CadastroDeFuncionarios.Tasks.Controller;
 
 import com.lorenzo.CadastroDeFuncionarios.Tasks.Service.TasksService;
 import com.lorenzo.CadastroDeFuncionarios.Tasks.dto.TasksDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,19 +22,29 @@ public class TasksController {
     }
 
     @PostMapping
-    public ResponseEntity<String> addTasks(@RequestBody TasksDTO dto) {
+    @Operation(summary = "Cria uma nova Tarefa", description = "Rota cria nova Tarefa e insere no Banco de Dados")
+    @ApiResponse(responseCode = "201", description = "Tarefa Criada com Sucesso")
+    @ApiResponse(responseCode = "400", description = "Erro ao Criar Tarefa")
+    public ResponseEntity<String> addTasks(@Parameter(description = "Usuario envia os dados da Tarefa a ser criada no corpo da requisicao") @RequestBody TasksDTO dto) {
         TasksDTO taskDTO = tasksService.addTasks(dto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Tarefa: " + taskDTO.getName() + " criada com sucesso | ID: " + taskDTO.getId());
     }
 
     @GetMapping
+    @Operation(summary = "Mostra todas as Tarefas cadastrados", description = "Rota mostra todas as Tarefas salvas no Banco de Dados")
+    @ApiResponse(responseCode = "200", description = "Mostrando tados as Tarefas")
+    @ApiResponse(responseCode = "404", description = "Nao ha Tarefas Existentes")
+    @ApiResponse(responseCode = "400", description = "Erro ao Mostrar Todas as Tarefas")
     public ResponseEntity<List<TasksDTO>> showAllTasks() {
         return ResponseEntity.ok(tasksService.showAllTasks());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> showTasksById(@PathVariable Long id) {
+    @Operation(summary = "Lista Tarefa pelo ID", description = "Rota Lista uma Tarefa pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Tarefa Encontrada")
+    @ApiResponse(responseCode = "404", description = "Tarefa Nao Encontrada")
+    public ResponseEntity<?> showTasksById(@Parameter(description = "Usuario envia o ID no Path da requisicao") @PathVariable Long id) {
         TasksDTO tasksDTO = tasksService.showTaskById(id);
         if (tasksDTO != null) {
             return ResponseEntity.ok(tasksDTO);
@@ -42,7 +55,11 @@ public class TasksController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateTask(@RequestBody TasksDTO dto, @PathVariable Long id) {
+    @Operation(summary = "Atualiza Tarefa Existente", description = "Rota atualiza Tarefa no Banco de Dados pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Tarefa Atualizada com Sucesso")
+    @ApiResponse(responseCode = "404", description = "Tarefa Nao Encontrada")
+    @ApiResponse(responseCode = "400", description = "Erro ao Atualizar Tarefa")
+    public ResponseEntity<String> updateTask(@Parameter(description = "Usuario envia os dados da Tarefa a ser atualizada no corpo da requisicao") @RequestBody TasksDTO dto, @Parameter(description = "Usuario envia o ID no Path da requisicao") @PathVariable Long id) {
         if (tasksService.showTaskById(id) != null) {
             dto.setId(id);
             tasksService.updateTask(dto, id);
@@ -53,7 +70,11 @@ public class TasksController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> removeTask(@PathVariable Long id) {
+    @Operation(summary = "Remove uma Tarefa", description = "Rota remove uma Tarefa do Banco de Dados pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Tarefa Removida com Sucesso")
+    @ApiResponse(responseCode = "404", description = "Tarefa Nao Encontrada")
+    @ApiResponse(responseCode = "400", description = "Erro ao remover Tarefa")
+    public ResponseEntity<String> removeTask(@Parameter(description = "Usuario envia o ID no Path da requisicao") @PathVariable Long id) {
         if (tasksService.showTaskById(id) != null) {
             tasksService.removeTask(id);
             return ResponseEntity.ok("Tarefa de id: " + id + " | Deletada com sucesso");
